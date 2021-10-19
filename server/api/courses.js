a7939babeed0eb6693051bd656dd7cb063d1f80e
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2021-10-19 14:28:49 -0400

    created markup for soul-runner-site

---
+++ server/api/courses.js
@@ -0,0 +1,35 @@
+export default async (req, res) => {
+  
+    return {
+      data: [
+        {title: "Grade 11 English",
+        description: "Expand your knowledge of English literature through the study of Shakespeare and Dickens.",
+        availableAt: ["St. Josephs", "St. Mary's", "St Dominic's"]
+      },
+      {title: "Grade 11 English",
+      description: "Expand your knowledge of English literature through the study of Shakespeare and Dickens.",
+      availableAt: ["St. Josephs", "St. Mary's", "St Dominic's"]
+    },
+    {title: "Grade 11 English",
+    description: "Expand your knowledge of English literature through the study of Shakespeare and Dickens.",
+    availableAt: ["St. Josephs", "St. Mary's", "St Dominic's"]
+  },
+  {title: "Grade 11 English",
+  description: "Expand your knowledge of English literature through the study of Shakespeare and Dickens.",
+  availableAt: ["St. Josephs", "St. Mary's", "St Dominic's"]
+},
+{title: "Grade 11 English",
+description: "Expand your knowledge of English literature through the study of Shakespeare and Dickens.",
+availableAt: ["St. Josephs", "St. Mary's", "St Dominic's"]
+},
+{title: "Grade 11 English",
+description: "Expand your knowledge of English literature through the study of Shakespeare and Dickens.",
+availableAt: ["St. Josephs", "St. Mary's", "St Dominic's"]
+},
+{title: "Grade 11 English",
+description: "Expand your knowledge of English literature through the study of Shakespeare and Dickens.",
+availableAt: ["St. Josephs", "St. Mary's", "St Dominic's"]
+},
+      ]}
+  }
+  
