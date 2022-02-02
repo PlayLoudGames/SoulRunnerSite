98938c61379e1e75ae292d1d84f72473e6975de8
Christopher Stevers <christopher.stevers1@gmail.com>
2022-02-02 09:22:01 -0500

    removed serverside deps

--- pages/adminst/index.vue
+++ pages/adminst/index.vue
@@ -1,18 +1,45 @@
 <script setup>
 import json from "../../public/data.json"
-import Adminui from "../../components/adminui.vue"
-useMeta({
-    meta: [
-        {script: "type='text/javascript' src='https://identity.netlify.com/v1/netlify-identity-widget.js'"}
-    ]
+import Adminui from "../../components/adminui.vue";
+import netlifyIdentity from 'netlify-identity-widget'
+import { onMounted} from "vue"
+onMounted(()=>{
+
+netlifyIdentity.init({ // defaults to document.body
+  locale: 'en', // defaults to 'en'
+});
+netlifyIdentity.on('open', () => console.log(netlifyIdentity.currentUser()));
+netlifyIdentity.open();
+
+// Get the current user:
+// Available after on('init') is invoked
+
+// Bind to events
+netlifyIdentity.on('init', user => console.log('init', user));
+netlifyIdentity.on('login', user => console.log('login', netlifyIdentity.currentUser()));
+netlifyIdentity.on('logout', () => console.log('Logged out'));
+netlifyIdentity.on('error', err => console.error('Error', err));
+netlifyIdentity.on('close', () => console.log('Widget closed'));
+
+// Unbind from events
+//netlifyIdentity.off('login'); // to unbind all registered handlers
+
+// Close the modal
+//netlifyIdentity.close();
+
+// Log out the user
+netlifyIdentity.logout();
+
+// Refresh the user's JWT
+// Call in on('login') handler to ensure token refreshed after it expires (1hr)  
+// Note: this method returns a promise.
 })
 
 </script>
 <template>
 
     <div>
-  <div data-netlify-identity-menu></div>
-        <Adminui :json="json" />
+       <Adminui :json="json" />
     </div>
 </template>
 
